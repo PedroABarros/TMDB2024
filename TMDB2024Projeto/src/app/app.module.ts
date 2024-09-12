d295503eeece400e9023fe3df287d12dcd7eaa21
PedroABarros <pedrokii.a.b@gmail.com>
2024-09-12 20:36:17 -0300

    Criação dos components da pagina sem o conteudo original

--- TMDB2024Projeto/src/app/app.module.ts
+++ TMDB2024Projeto/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FilmesComponent } from './filmes/filmes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    PrincipalComponent,
+    FilmesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ TMDB2024Projeto/src/app/filmes/filmes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filmes',
+  templateUrl: './filmes.component.html',
+  styleUrl: './filmes.component.scss'
+})
+export class FilmesComponent {
+
+}
